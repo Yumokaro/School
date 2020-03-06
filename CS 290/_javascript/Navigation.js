dd2c735b182e2beabba04702740a72b73fea574f
Yumokaro <temporaryfrost@outlook.com>
2020-03-06 10:48:20 -0800

    work on js create nav bar js file

---
+++ CS 290/_javascript/Navigation.js
@@ -0,0 +1,6 @@
+/* Navigation Functions */
+
+function open_Nav_Bar() {
+    document.getElementsByClassName("side_Nav_Bar").style.maxWidth = "30%";
+    document.getElementsByClassName("main_Body").style.marginLeft = "5%";
+}
