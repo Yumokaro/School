e17b6b5a43d3068e718e1694c1a4dec9fe8baebb
Yumokaro <temporaryfrost@outlook.com>
2020-03-08 10:48:35 -0700

    base side nav

--- CS 290/_javascript/Navigation.js
+++ CS 290/_javascript/Navigation.js
@@ -1,8 +1,8 @@
 /* Navigation Functions */
 
 function open_Nav_Bar() {
-    document.getElementById("side_Nav").style.width = "250px";
-    document.getElementById("main_Body").style.marginLeft = "250px";
+    document.getElementById("side_Nav").style.width = "25%";
+    document.getElementById("main_Body").style.marginLeft = "100px";
 }
 
 function close_Nav_Bar() {
