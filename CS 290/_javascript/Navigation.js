655c8ab279294b92ee5a5620c0419935a0e46dd2
Yumokaro <temporaryfrost@outlook.com>
2020-03-08 16:55:37 -0700

    nav done for now

--- CS 290/_javascript/Navigation.js
+++ CS 290/_javascript/Navigation.js
@@ -1,8 +1,8 @@
 /* Navigation Functions */
 
 function open_Nav_Bar() {
-    document.getElementById("side_Nav").style.width = "15%";
-    document.getElementById("main").style.marginLeft = "15%";
+    document.getElementById("side_Nav").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
 }
 
 function close_Nav_Bar() {
