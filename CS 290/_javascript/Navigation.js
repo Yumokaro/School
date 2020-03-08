f79a27165ff1d96fce718513ee5ee19ae65a99c7
Yumokaro <temporaryfrost@outlook.com>
2020-03-08 10:45:59 -0700

    side nav base add to windows and linux

--- CS 290/_javascript/Navigation.js
+++ CS 290/_javascript/Navigation.js
@@ -1,6 +1,11 @@
 /* Navigation Functions */
 
 function open_Nav_Bar() {
-    document.getElementsByClassName("side_Nav_Bar").style.maxWidth = "30%";
-    document.getElementsByClassName("main_Body").style.marginLeft = "5%";
+    document.getElementById("side_Nav").style.width = "250px";
+    document.getElementById("main_Body").style.marginLeft = "250px";
+}
+
+function close_Nav_Bar() {
+    document.getElementById("side_Nav").style.width = "0";
+    document.getElementById("main_Body").style.marginLeft= "0";
 }
