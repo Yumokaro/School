d048d1a6b47c7040ca3135afc5014167ce60196e
Yumokaro <temporaryfrost@outlook.com>
2020-03-10 14:29:07 -0700

    update

--- cs290react/src/App.js
+++ cs290react/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-/* import logo from './logo.svg'; */
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+class App extends Component {
+  render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Route  path="/" component={HomePage} exact />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
